609d720ccd0819d85a0259019f9a120a8d0baa41
joshuatree1974 <josuearranhado@gmail.com>
2026-03-23 19:53:17 +0000

    Fim Do Projeto

---
+++ fluxograma02/Program.cs
@@ -0,0 +1,80 @@
+﻿
+
+namespace Fluxograma02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           Console.WriteLine("1. INICIO");
+           Console.WriteLine("2. COMECEI A TRABALHAR");
+           Console.WriteLine("3. RECEBI 10 DOCUMENTOS");
+           int documentos =2;
+           int contador =0;
+
+
+           Console.WriteLine("4.AINDA TEM DOCUMENTOS PARA ASSINAR E AUDITAR?");
+
+
+
+           while (contador < documentos)
+           {
+
+
+                 Console.WriteLine(" SIM - > AUDITANDO E ASSINANDO ");
+                 Console.WriteLine($"DOCUMENTOS {contador + 1} CONCLUINDO");
+
+                 
+
+                 contador++;
+
+           }    
+
+
+             Console.WriteLine("TODOS OS DOCUMENTOS FORAM CONCLUÍDOS!");
+
+             Console.WriteLine(" JA NAO TEM DOCUMENTOS ");
+             Console.WriteLine("AINDA TEM ATIVIDADE");
+
+
+             bool temAtividade = true;
+
+
+
+             if (temAtividade == true)
+             {
+                Console.WriteLine("SIM TEM ATIVIDADE");  
+             }
+             else 
+             {
+                Console.WriteLine("NAO TEM ATIVIDADE");     
+             }     
+     
+               
+             Console.WriteLine("6. FIM"); 
+
+             Console.WriteLine("\n*******FIM DO PROGRAMA********"); 
+        }
+
+    }    
+
+}
+
+
+
+
+
+
+
+           
+
+
+
+
+
+
+        
+    
+
+
